ad179020e287b6c61126164ab1bfd01b7815b5c0
Daingab <47778568+Daingab@users.noreply.github.com>
2019-08-27 23:17:57 +0200

    Added Scripts folder to Master

---
+++ Assets/Scripts/SoundTesting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTesting : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //source.PlayOneShot(clip);
+    }
+}
