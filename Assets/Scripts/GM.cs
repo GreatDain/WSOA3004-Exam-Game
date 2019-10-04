e501e0b79b460e83ebdfa378eb4067a70187ae34
Daingab <47778568+Daingab@users.noreply.github.com>
2019-10-04 09:28:14 +0200

    Health UI implemented

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GM : MonoBehaviour
 {
+    public int health;
+    public int numberOfGorillas;
+
+    public Image[] gorillas;
+    public Sprite fullHP;
+    public Sprite emptyHP;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -16,6 +23,33 @@ public class GM : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Quit();
+        }
+
+        if (health > numberOfGorillas)
+        {
+            health = numberOfGorillas;
+        }
+
+        for (int i = 0; i < gorillas.Length; i++)
+        {
+            if (i < health)
+            {
+                gorillas[i].sprite = fullHP;
+            }
+            else
+            {
+                gorillas[i].sprite = emptyHP;
+            }
+
+            if (i < numberOfGorillas)
+            {
+                //print(i);
+                gorillas[i].enabled = true;
+            }
+            else
+            {
+                gorillas[i].enabled = false;
+            }
         }
     }
 
