277e41874888fd31650c90dbb998c6c88b5c6eeb
Daingab <47778568+Daingab@users.noreply.github.com>
2019-10-21 21:46:28 +0200

    Changes to circus level design.

---
+++ Assets/Scripts/EnemyController/EnemyGroundDetect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroundDetect : MonoBehaviour
+{
+    public bool onGrass;
+    public bool onGravel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Grass")
+        {
+            onGrass = true;
+        }
+
+        if (other.gameObject.tag == "Gravel")
+        {
+            onGravel = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Grass")
+        {
+            onGrass = false;
+        }
+
+        if (other.gameObject.tag == "Gravel")
+        {
+            onGravel = false;
+        }
+    }
+}
