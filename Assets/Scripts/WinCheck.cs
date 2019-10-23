77f555c82ac2321a655f2f17d980256c300f6fbf
FalseCrystal <kakitleong8@gmail.com>
2019-10-23 12:57:37 +0200

    Added changes for win condition

---
+++ Assets/Scripts/WinCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCheck : MonoBehaviour
+{
+
+    public GameObject winText;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+
+            StartCoroutine(delayReturn());
+
+        }
+    }
+
+    IEnumerator delayReturn()
+    {
+
+        winText.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene("Menu");
+    }
+}
