1c3d0171988b488a410ab7b924f6d61c0eeb0454
Daingab <47778568+Daingab@users.noreply.github.com>
2019-10-23 12:59:11 +0200

    Added controls testing scene and minor adjustments to game scene.

--- Assets/Scripts/MenuCC.cs
+++ Assets/Scripts/MenuCC.cs
@@ -6,9 +6,21 @@ using UnityEngine.SceneManagement;
 public class MenuCC : MonoBehaviour
 {
 
+    private float speedNorm = 15f;
+    private float sprint = 30f;
     private float speed = 15f;
-
+    private float sneak = 8f;
     public Animator walkCycle;
+    public bool cooldown = false;
+    public float sprintTime;
+    private float sprintDuration = 5f;
+    public bool isSprint = false;
+    public bool isSneak = false;
+    public float stableVol;
+    public AudioSource audioSource;
+    public bool isGrounded = false;
+    public GameObject controlsPanel;
+    public bool controlsEnabled = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +30,9 @@ public class MenuCC : MonoBehaviour
         walkCycle.SetBool("isStrafeRight", false);
         walkCycle.SetBool("walkToStrafeL", false);
         walkCycle.SetBool("walkToStrafeR", false);
+
+        stableVol = audioSource.volume;
+
     }
 
     // Update is called once per frame
@@ -84,21 +99,124 @@ public class MenuCC : MonoBehaviour
             walkCycle.SetBool("walkToStrafeL", false);
             walkCycle.SetBool("walkToStrafeR", false);
         }
+
+        if (Input.GetKey(KeyCode.LeftShift) && cooldown == false)
+        {
+            sprintTime += Time.deltaTime;
+        }
+        else if (cooldown == true)
+        {
+            sprintTime -= Time.deltaTime;
+        }
+
+        if (sprintTime <= 0)
+        {
+            cooldown = false;
+        }
+
+        //Adds sprint functionality. Speeds up animation accordingly.
+        if (!isSprint && translation > 0 && Input.GetKeyDown(KeyCode.LeftShift) && cooldown == false)
+        {
+            speed = sprint;
+            isSprint = true;
+            audioSource.volume = 0.9f;
+            stableVol = audioSource.volume;
+            walkCycle.speed = 2f;
+        }
+        else if (isSprint && (Input.GetKeyUp(KeyCode.LeftShift) || (sprintTime > sprintDuration)))
+        {
+            speed = speedNorm;
+            isSprint = false;
+            audioSource.volume = stableVol;
+            walkCycle.speed = 1f;
+            cooldown = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            audioSource.volume = 0.1f;
+            speed = sneak;
+            isSneak = true;
+            stableVol = audioSource.volume;
+            walkCycle.speed = 0.5f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = speedNorm;
+            isSneak = false;
+            audioSource.volume = stableVol;
+            walkCycle.speed = 1f;
+        }
+
+        if (isSprint == true || cooldown == true)
+        {
+            audioSource.pitch = 1.8f;
+            audioSource.volume = 0.7f;
+        }
+        else if (isSneak == true)
+        {
+            audioSource.pitch = 0.8f;
+            audioSource.volume = 0.3f;
+        }
+        else
+        {
+            audioSource.pitch = 1.15f;
+            audioSource.volume = 0.5f;
+        }
+
+        sprintTime = Mathf.Clamp(sprintTime, 0, sprintDuration);
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(controlsEnabled == true)
+            {
+                controlsEnabled = false;
+                controlsPanel.SetActive(false);
+            }
+            else if (controlsEnabled == false)
+            {
+                controlsEnabled = true;
+                controlsPanel.SetActive(true);
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.name == "Play")
+        //Detects if the player is on the ground.
+        if (other.gameObject.tag == "Grass" || other.gameObject.tag == "Gravel")
         {
-            SceneManager.LoadScene(1);
+            isGrounded = true;
         }
-        else if (other.gameObject.name == "Controls" && Input.GetKeyDown(KeyCode.E))
+
+        //Detects what terrain the player is walking on (either gravel or grass and sets sounds conditions accordingly.
+        if (other.gameObject.tag == "Grass" && isSneak == false && isSprint == false)
         {
-            print("Do something");
+            audioSource.volume = 0.4f;
+            stableVol = audioSource.volume;
+            //isGrounded = true;
         }
-        else if (other.gameObject.name == "Quit" && Input.GetKeyDown(KeyCode.E))
+        else if (other.gameObject.tag == "Gravel" && isSneak == false && isSprint == false)
         {
-            Application.Quit();
+            audioSource.volume = 0.6f;
+            stableVol = audioSource.volume;
+            //isGrounded = true;
         }
     }
-}
+
+        /*private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "Play")
+            {
+                SceneManager.LoadScene(1);
+            }
+            else if (other.gameObject.name == "Controls" && Input.GetKeyDown(KeyCode.E))
+            {
+                print("Do something");
+            }
+            else if (other.gameObject.name == "Quit" && Input.GetKeyDown(KeyCode.E))
+            {
+                Application.Quit();
+            }
+        }*/
+    }
