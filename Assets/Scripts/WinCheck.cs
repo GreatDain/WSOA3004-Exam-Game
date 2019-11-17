54aba89212c51ccae38a02aa20fd904ae50903f6
MarmaladeJam <mamelloraboroko8.mr@gmail.com>
2019-11-18 00:48:56 +0200

    Added Win Particles

--- Assets/Scripts/WinCheck.cs
+++ Assets/Scripts/WinCheck.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 
 public class WinCheck : MonoBehaviour
 {
-   public Text winText;
-   public GameObject promptTicket;
+    public Text winText;
+    public GameObject promptTicket;
+    public GameObject winConfetti;
+    public Transform confettiSpawnPoint;
 
     // Start is called before the first frame update
 
@@ -38,6 +40,7 @@ public class WinCheck : MonoBehaviour
 
         winText.enabled = true;
         promptTicket.SetActive(true);
+        Instantiate(winConfetti, confettiSpawnPoint.position, Quaternion.identity);
 
         yield return new WaitForSeconds(5);
 
